819a16956b1b41c2b3eaa1e3084c0c38b56d5e54
Erickson Vásquez <45046723+ericksonvasquez@users.noreply.github.com>
2024-02-12 19:32:55 -0400

    Create StartDB.js

---
+++ StartDB.js
@@ -0,0 +1,18 @@
+const AWS = require("aws-sdk");
+
+exports.handler = async (event) => {
+
+    AWS.config.update({ region: 'us-east-1' });
+
+    const rds = new AWS.RDS();
+    
+    const dbInstanceIdentifier = process.env.DB_INSTANCE_IDENTIFIER;
+
+    if (!dbInstanceIdentifier) {
+        return { statusCode: 400, body: "DB_INSTANCE_IDENTIFIER environment variable is not set" };
+    }
+
+        await rds.startDBInstance({ DBInstanceIdentifier: dbInstanceIdentifier }).promise();
+
+    return { statusCode: 200, body: "Operation completed successfully" };
+};
